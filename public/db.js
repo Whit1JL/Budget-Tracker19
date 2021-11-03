7d40eefca302555773ef2955060846d0feed1ce3
Jessica White <jessica2714white@gmail.com>
2021-11-02 19:01:43 -0500

    Connect mongoose

--- public/db.js
+++ public/db.js
@@ -1,13 +1,3 @@
 const indexedDB 
-
-mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost/budget-trackerpwa19',
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false
-    }
-  );
   
 // tutor says DB is referred here
